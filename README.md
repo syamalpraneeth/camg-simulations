d2c0ec23b2afff6916d5100f263885cb87a5aa39
syamal.chilakalapudi <syamal.chilakalapudi@kit.edu>
2020-04-09 13:45:43 +0200

    files given by Dr. Penghui Cao UCI
    Former-commit-id: 0db6e75701d3497e338f246e90a3063da351f2c8

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-This folder contains scripts relevamt to projects that were developed during the thesis work of Mr. Syamal Praneeth Chilakalapudi.
+This folder contains scripts relevant to projects that were developed during the thesis work of Mr. Syamal Praneeth Chilakalapudi.
 
 Over time this must evolve into a repository that is synced to the git.scc.kit.eu Gitlab portal for future reference of Ph.D. students.
+
+Branch CIBD Penghui: Contains initial scripts Penghui gave me which will in later commits be shown to have been modified and improved.
---
+++ cibd/step1/post/plot_log.py
@@ -0,0 +1,31 @@
+import os #, sys
+# simple test of log tool
+
+lg = log("../log.lammps")
+
+g = gnu()
+g.erase()				#reset all attributes to default values
+g("set terminal png size 600,400 enhanced font 'Verdana,10'")
+g("set output 'comp.png'")
+#g.aspect(1.3)				#aspect ratio
+g("set ylabel 'Composition'")
+g("set xlabel 'Radius (Angstroms)'")
+g("set title 'Composition (radial)'")	#title text
+g("plot 'tmp.log.comp' using 1:2 title 'Cu', 'tmp.log.comp' using 1:3 title 'Zr'")
+g.stop()
+
+
+#os.remove("tmp.log.two")
+#os.remove("tmp.log.count")
+#os.remove("tmp.log.temp")
+#os.remove("tmp.log.radius")
+#os.remove("tmp.log.comp")
+#os.remove("tmp.log.pe")
+#os.remove("molfil.txt")
+
+#sys.command("sys.exit()")
+#print "all done ... type CTRL-D to exit Pizza.py"
+#sys.exit() ===> raises exception. So I used below line from link: https://stackoverflow.com/questions/173278/is-there-a-way-to-prevent-a-systemexit-exception-raised-from-sys-exit-from-bei
+os._exit(1)
+
+
