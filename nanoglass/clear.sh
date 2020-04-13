eabdcd5d8a9deec8e2cf1eeeacb0b5c21f3b4933
syamal.chilakalapudi <syamal.chilakalapudi@kit.edu>
2020-04-13 17:01:36 +0200

    /post scripts work, in.lattice remains as most recent version
    Former-commit-id: 0e3b3164767bd95f33b17569b840d52d556e1e80

---
+++ nanoglass/clear.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+##################################################################
+##								##                               
+##								##
+##Dependencies	:						##
+##Influences	:						##
+##################################################################
+## ver.	: 2019--, Syamal Praneeth Chilakalapudi, KIT, INT	##                            
+##Author Email    :syamalpraneeth@gmail.com			##
+##################################################################
+
+rm -rf in.* data/ *.out *log* post/ restart.* *.py*
--- nanoglass/post/proc.sh
+++ nanoglass/post/proc.sh
@@ -1,16 +1,12 @@
-!/bin/sh
+#!/bin/sh
 
-echo "Change file name according to size of system!"
+sed '1,4d' ../data/rdf_0GPa.rdf > tmp.rdf_0GPa
+sed '1,4d' ../data/rdf_6GPa.rdf > tmp.rdf_6GPa
+sed '1,4d' ../data/rdf_500K.rdf > tmp.rdf_500K
 
-sed '1,4d' ../data/rdf_0GPa_6nm.rdf > tmp.rdf_0GPa
-sed '1,4d' ../data/rdf_6GPa_6nm.rdf > tmp.rdf_6GPa
-sed '1,4d' ../data/rdf_500K_6nm.rdf > tmp.rdf_500K
-#sed '1,4d' ../data/rdf_50K_6nm.rdf > tmp.rdf_50K
-
-sed '1,9d' ../data/dump.voronoi_0GPa_6nm | awk '{print $7}' > tmp.voronoi_0GPa
-sed '1,9d' ../data/dump.voronoi_6GPa_6nm | awk '{print $7}' > tmp.voronoi_6GPa
-sed '1,9d' ../data/dump.voronoi_500K_6nm | awk '{print $7}' > tmp.voronoi_500K
-#sed '1,9d' ../data/dump.voronoi_50K_6nm | awk '{print $7}' > tmp.voronoi_50K
+sed '1,9d' ../data/dump.voronoi_0GPa | awk '{print $7}' > tmp.voronoi_0GPa
+sed '1,9d' ../data/dump.voronoi_6GPa | awk '{print $7}' > tmp.voronoi_6GPa
+sed '1,9d' ../data/dump.voronoi_500K | awk '{print $7}' > tmp.voronoi_500K
 
 chmod u+x plot_log.py post.py
 
