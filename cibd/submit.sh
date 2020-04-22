37bf1a6bd3ccfbb18c592a2228ceb29eb653ebfd
syamal.chilakalapudi <syamal.chilakalapudi@kit.edu>
2020-04-23 00:21:41 +0200

    Manually copied in changes from branch uci-cibd development. Now the files are updated until the end of my trip to UCI

---
+++ cibd/submit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+##################################################################
+##                                                              ##
+##                                                              ##
+##Dependencies  :                                               ##
+##Influences    :                                               ##
+##################################################################
+## ver. : 2019--, Syamal Praneeth Chilakalapudi, KIT, INT       ##
+##Author Email    :syamalpraneeth@gmail.com                     ##
+##################################################################
+
+v=3	#size of cluster diameter
+sub=24	#size of substrate edge
+sbatch --job-name="cibd_cuzr_"$v"nm" --output="cibd_multi_"$v"nm_%aeV".out --export=ALL,v=$v,sub=$sub job.arr.script 
+
