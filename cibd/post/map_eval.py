ec7bb82623846a765e8a01617d8f14eb509bf458
syamal.chilakalapudi <syamal.chilakalapudi@kit.edu>
2020-04-24 16:56:51 +0200

    post.sh to modify lammps data file and map eval python script to generate a datafile for heat map. Makes F(x,y) data
    Former-commit-id: 823d021a921fae2dc220844e42dc80f388803ee0

--- cibd/post/map_eval.py
+++ cibd/post/map_eval.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python2.7
-import sys.os
+from __future__ import division
+
+import sys,os
+import numpy as np
+
 ##################################################################
 ##MAP_EVAL.PY							##                               
 ##								##
@@ -10,48 +14,99 @@ import sys.os
 ##Author Email    :syamalpraneeth@gmail.com			##
 ##################################################################
 
-xhi=sys.argv[0]
-xlo=sys.argv[1]
-yhi=sys.argv[2]
-ylo=sys.argv[3]
-dt=sys.argv[4]
+xlo=float(sys.argv[1])
+xhi=float(sys.argv[2])
+ylo=float(sys.argv[3])
+yhi=float(sys.argv[4])
+dt=int(sys.argv[5])
+z=int(sys.argv[6])
+st=sys.argv[7]
+
+def myrange(start, end, step):
+    while start <= end:
+        yield start
+        start += step
+
+def truncate(n, decimals=0):
+    multiplier = 10 ** decimals
+    return int(n * multiplier) / multiplier
+
+
+def ratio_eval(x,y,z,dt,mlist = []):
+	xp=x+dt
+	yp=y+dt
+	dt2=dt/2
+	zl=z-dt2	#floor the z, please!
+	zh=z+dt2
+	cu=0
+	zr=0
+	for a in myrange(x,xp,1):
+		for b in myrange(y,yp,1):
+			for c in myrange(zl,zh,1):
+#				filsiz=len(mlist)
+				for it in mlist:
+					t=it[0]
+					h=it[1]
+					k=it[2]
+					l=it[3]
+					if (h>=x and h<=xp and k>=y and k<=yp and l>=zl and l<=zh):
+					#if coords fall within this box
+						if ((t == 1) or (t ==3)): cu=cu+1
+						if ((t == 2) or (t ==4)): zr=zr+1
+#	print(cu,zr)
+	if ((cu == 0) and (zr == 0)):
+		r= -1
+	else:
+		r = cu/(cu+zr)
+		r = truncate(r,3)
+	print(r)
+	return r
+
+t       = open(st+'/tmp.coords','r')
+#molfil.txt was written as: write_dump  all custom molfil.txt id type x y z f_ff3 in lammps run files
+lines   = t.readlines()
+t.close()
+
+#Remove a bunch of lines and, read coordinates & type
+#del lines[0:9]
+a = np.array(lines)
+mat = np.empty([len(lines),len(a[1].split())])
+
+for i in range(0,len(lines)):
+        mat[i] = a[i].split()
+#print(mat)
+
 #At a given z, we take a rectangular sheet and divide into cubes of sheet thickness
-z=0 #At the given value of z
+#At the given value of z
 
 #then, within the given cube, we evaluate the ratio: #cu/(#cu+#zr). ratio value b/w 0 and 1
 #
 #if #cu and #zr both are 0, then there is a void        : assign ratio -1
 #if #cu == 0, then there is no Copper                   : assign ratio 0
 #if #zr == 0, then its a pure copper region             : assign ratio 1
-
-for i in range(xlo,xhi,dt):
-	for j for in range(ylo,yhi,dt):
-		 #write x,y,f values to file
-
-
-
+#print(len(lines))
+ratio=[] #np.empty([len(lines),4])
+i=0
+#print(mat)
+val=np.empty([1,4])
+print(val)
+for x in myrange(np.amin(mat[:,1]),np.amax(mat[:,1]),dt):
+	for y in myrange(np.amin(mat[:,2]),np.amax(mat[:,2]),dt):
+		for c in range(1,dt):
+			val[0,0]=x
+			val[0,1]=y
+			val[0,2]=z
+			val[0,3]=ratio_eval(x,y,z,dt,mat)
+			ratio.append(val)
+#			print(ratio)
+#			print(val)
+#		print(i,ratio[i,:])
+		with open("test.txt","a") as f:
+			line = str(x)+","+str(y)+","+str(val[0,3])+'\n'	#write x,y,f values to file
+			f.write(line)	
+a=np.array(ratio)
+print(a)
 #assign volume of the cube a colour based on ratio
 #scatter plot at given heights
 
-def my_range(start, end, step):
-    while start <= end:
-        yield start
-        start += step
 
-def ratio_eval(x,y,z):
-	xp=x+6
-	yp=y+6
-	zh=z-3	#floor the z, please!
-	xl=z+3
-	for a in myrange(x,xp,1):
-		for b in myrange(y,yp,1):
-			for c in myrange(zl,zh,1):
-				for <file line iteration>:
-					if (h>x & h<xp & k>y & k<yp & l>zl & l<zh):
-						if (<> == 1 | ==3): count copper
-						if (<> == 2 | ==4): count zirconium
-	
-	if (zr neq 0):
-		r= cu/(cu+zr)
-	else
-		r=-1
--- cibd/post/post.sh
+++ cibd/post/post.sh
@@ -10,24 +10,28 @@
 ##################################################################
 
 
-DATA_FIL=~/Documents/work/simulations/projects/cibd_cuzr_mutli_fh2/uci_cibd/data/data.cibd_film
+DATA_FIL=~/Documents/work/simulations/projects/cibd_cuzr_mutli_fh2/uci_cibd/data/data.cibd_film_z-55_new
 OUTPUT_DIR=~/Documents/work/simulations/projects/cibd_cuzr_mutli_fh2/uci_cibd/data
 
 xlo=-109 #-109.7488709	#bound values taken manually from $DATA_FIL
 xhi=109  #109.7488709
 ylo=-109 #-109.7488709
 yhi=109 #109.7488709
-zlo=-22 #-22.1725197
-zhi=193 #193.7332764
-num_atoms=205337	#number of atoms taken from $DATA_FIL manually
+zlo=51 #-22 #-22.1725197
+zhi=59 #193 #193.7332764
+
+z=55
+nat=2793	#number of atoms taken from $DATA_FIL manually
 dt=6			#6 angstroms thickness cube
 #xl0=0
 #xhi=13
 xh=`echo "var=$xhi - $xlo;var=var- $xhi - var % $dt ;var" | bc`
 yh=`echo "var=$yhi - $ylo;var=var- $yhi - var % $dt ;var" | bc`
 
-sed '1,9d' $DATA_FIL | awk '{print $2 " "  $3 " " $4 " " $5}' > tmp.coords	#at type and coords
+sed '1,9d' $DATA_FIL | awk '{print $2 " "  $3 " " $4 " " $5}' > tmp
+sed '1,2793!d' tmp > $OUTPUT_DIR/tmp.coords	#atom type and coords
+rm -rf tmp
 
-python map_eval.py $xlo $xhi $ylo $yhi $dt
+python map_eval.py $xlo $xhi $ylo $yhi $dt $z $OUTPUT_DIR
 
 
