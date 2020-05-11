8582814d55c44bfbf4b408742ba3df68687c1d70
Chilakalapudi <mj0054@int-nano.int.kit.edu>
2020-05-12 01:14:03 +0200

    hcp coord genetrator works for one layer

--- cibd/gen_fcc_coords.py
+++ cibd/gen_fcc_coords.py
@@ -1,4 +1,4 @@
-#!/home/ws/mj0054/local/anaconda3/bin/python
+#!/home/Documents/local/anaconda3/bin/python
 from __future__ import division
 import math
 import numpy as np
@@ -13,7 +13,7 @@ import numpy as np
 ##Author Email    :syamalpraneeth@gmail.com			##
 ##################################################################
 
-r=3
+r=1.5
 
 #for i in range(1,5):
 #	if i%2==0: siz=3
@@ -23,26 +23,40 @@ r=3
 #			x=x+4*r
 #			y=y+4*r
 
-a=np.zeros((4,64))
+a=np.zeros((68,3))	# 8x5 + 7x4
 
 y=r
-for i in range(1,9):
-	x=r
-	y=y+(math.sqrt(3)*r)
+ind=0
+for i in range(1,10):
 	for l1 in range(0,2):
+		x=r
 		if i%2==0:
-			siz=4-l1
-			ix=2
+			if l1==1:
+				siz=4-l1
+			ix=1
 			x=x+r
 		else:
 			siz=4
-			ix=1
-		for cnt in range(0,siz):
-			x=x+4*r
-#			ind=(i-1)+siz*(cnt-1)
-			a[ind,1]=ix+l1
-			a[ind,2]=x
-			a[ind,3]=y
-			ind+=1
+			ix=0
+		if l1==1: x=x+2*r
 
-print(a)
+		for cnt in range(0,siz):
+#			ind=(i-1)+siz*(cnt-1)
+			a[ind,0]=2*ix+l1+1
+#			print(a[ind,0])
+			a[ind,1]=x
+			a[ind,2]=y
+			x=x+4*r
+			print(str(i)+','+str(ind)+','+str(a[ind,0]))
+			ind+=1
+	y=y+(math.sqrt(3)*r)
+
+
+#rint(a)#
+
+matrix=np.matrix(a)
+#rint(matrix)
+with open('outfile.txt','wb') as f:
+	for line in matrix:
+		np.savetxt(f, line, fmt='%.2f')
+
