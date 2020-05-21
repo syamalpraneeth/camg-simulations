b77a1edb3b36ae40ec740d9e5e09078b4cf28223
syamal.chilakalapudi <syamal.chilakalapudi@kit.edu>
2020-05-21 13:26:26 +0200

    Added test cases for heatmap evaluation; also can make videos of heatmap now

--- cibd/post/map_eval.py
+++ cibd/post/map_eval.py
@@ -8,6 +8,7 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 from matplotlib import colors
+np.set_printoptions(threshold=sys.maxsize)
 
 ##################################################################
 ##MAP_EVAL.PY							##                               
@@ -26,7 +27,8 @@ ylo=float(sys.argv[3])
 yhi=float(sys.argv[4])
 dt=int(sys.argv[5])
 z=int(sys.argv[6])
-st=sys.argv[7]
+filno=str(sys.argv[7])
+st=sys.argv[8]
 
 #defining custom functions
 def myrange(start, end, step):	#custom range function for arbitary step values
@@ -38,42 +40,13 @@ def truncate(n, decimals=0):	#truncate decimals
     multiplier = 10 ** decimals
     return int(n * multiplier) / multiplier
 
-
-def ratio_eval(x,y,z,dt,mlist = []): #evaluate ratio of cu to TOTAL atoms
-	xp=x+dt
-	yp=y+dt
-	dt2=dt/2
-	zl=z-dt2
-	zh=z+dt2
-	cu=0
-	zr=0
-	for a in myrange(x,xp,1):
-		for b in myrange(y,yp,1):
-			for c in myrange(zl,zh,1):
-#				filsiz=len(mlist)
-				for it in mlist:
-					t=it[0]
-					h=it[1]
-					k=it[2]
-					l=it[3]
-					if (h>=x and h<=xp and k>=y and k<=yp and l>=zl and l<=zh):
-					#if coords fall within this box
-						if ((t == 1) or (t ==3)): cu=cu+1
-						if ((t == 2) or (t ==4)): zr=zr+1
-#	print(cu,zr)
-	if ((cu == 0) and (zr == 0)):
-		r= -1
-	else:
-		r = cu/(cu+zr)
-		r = truncate(r,3)
-#	print(r)
-	return r
-
 #snippet modified from: https://www.geeksforgeeks.org/python-program-for-binary-insertion-sort/
 def find_ge(a, x): #Find leftmost item greater than or equal to x
 	i = bisect.bisect_left(a, x)
- #   if i != len(a):
-	return i
+#	print(i)
+	if x==a[i]:
+		return i+1
+	else: return i
  #   raise ValueError
 
 #------------------------------------------------------------------------------------End of functions
@@ -86,85 +59,84 @@ a = np.array(lines)
 mat = np.empty([len(lines),len(a[1].split())])
 for i in range(0,len(lines)):
         mat[i] = a[i].split()
-#print(mat) #.shape)
+#print(mat.shape)
 
-""" print commands block
-print(xlo)
-print(xhi)
-print(ylo)
-print(yhi)
-"""
+cmat = np.array(mat)
 
-prod = int(((xhi-xlo)//(dt))*((yhi-ylo)//(dt)))
+dt2=dt/2
+count=0
+ndels=0
+zma=z+dt2
+zmi=z-dt2
+iter=0
+
+#print(zmi,z,zma)
+
+#----------------------------------------------SEPARATE OUT PARTICLES IN Z-RANGE
+entry=[]
+#print(mat)
+for index in cmat:
+        if (index[3]<zmi or index[3]>zma):
+                entry=np.append(entry,iter)
+        iter+=1
+
+#print(np.shape(entry))
+#print(np.shape(mat))
+mat=np.delete(mat,entry,axis=0)
+#print(np.shape(mat))
+#print(mat)
+
+#----------------------------------------------SORT PARTICLES INTO A GRID
+prod = int((1+(xhi-xlo)//(dt))*(1+(yhi-ylo)//(dt)))
 #print(prod)
 #print(np.amin(mat[:,1]))
 #print(np.amax(mat[:,1]))
 grid = np.zeros((prod,4)) #making a coordinate grid
-rat = np.full((prod,1),-2) #making a column for ratios
+rat = np.full((prod,1),-1) #making a column for ratios
 grid=np.append(grid,rat,axis=1)
 #print(np.shape(grid))
 #print(np.shape(rat))
 #print(grid)
 
+xsp=np.arange(xlo+dt2,xhi+dt2,dt)
+ysp=np.arange(ylo+dt2,yhi+dt2,dt)
+xsp=np.arange(xlo,xhi+dt,dt)
+ysp=np.arange(ylo,yhi+dt,dt)
+asda=np.meshgrid(xsp,ysp)
+#print(asda)
+#print(np.shape(asda))
+#print(xlo)
+#print(xhi)
+#print(ylo)
+#print(yhi)
+#print(xsp)
+#print(ysp)
+#print(np.shape(ysp))
+
 cnt=int(0)
+cnty=int(0)
 #At a given z, we take a rectangular sheet and divide into cubes of sheet thickness
-for x in myrange(xlo,xhi-dt,dt):
-	for y in myrange(ylo,yhi-dt,dt):
+for x in xsp:
+	for y in ysp:
 #		print(cnt,x,y)
 #		print(cnt)
 #		grid[cnt,0] = int(cnt)
 		grid[cnt,0] = float(x)
 		grid[cnt,1]= float(y)
 		cnt+=1
+		cnty+=1
+#	print(cnty)
 #print(np.shape(grid))
 #print(grid)
-
-"""
-#then, within the given cube, we evaluate the ratio: #cu/(#cu+#zr). ratio value b/w 0 and 1
-#
-#if #cu and #zr both are 0, then there is a void        : assign ratio -1
-#if #cu == 0, then there is no Copper                   : assign ratio 0
-#if #zr == 0, then its a pure copper region             : assign ratio 1
-#print(len(lines))
-#ratio=[] #np.empty([len(lines),4])
-
-i=0
-#print(mat)
-val=np.empty([1,4])
-#print(val)
-
-for x in myrange(np.amin(mat[:,1]),np.amax(mat[:,1]),dt):
-	for y in myrange(np.amin(mat[:,2]),np.amax(mat[:,2]),dt):
-	#	for c in range(1,dt):
-		val[0,0]=x+(dt/2)
-		val[0,1]=y+(dt/2)
-		val[0,2]=z
-#		val[0,3]=ratio_eval(x,y,z,dt,mat)
-		ratio.append(val)
-#		print(ratio)
-#		print(val)
-#		print(i,ratio[i,:])
-		with open("test.txt","a") as f:
-			line = str(x)+","+str(y)+","+str(val[0,3])+'\n'	#write x,y,f values to file
-			f.write(line)
-		i=i+1
-	print(i)
-a=np.array(ratio)
-#print(a)
-"""
-
-xsp=np.arange(xlo,xhi,dt)
-ysp=np.arange(ylo,yhi,dt)
-cu = np.ones((prod,1))
-zr = np.zeros((prod,1))
 
 #iterate over the matrix of coords and atom types, allocate which grid they belong to and count total atoms in the grid.
 itera=0
 for c in mat:
+#	print(c)
 	a=find_ge(xsp,c[1]) #abscissa look up
 	b=find_ge(ysp,c[2]) #ordinate look up
-#	index=int(a+((b)*((xhi-xlo)//(dt))))
-	index=int((b-1)+((a-1)*((xhi-xlo)//(dt))))
+	index=int(b-1+((a-1)*(1+(xhi-xlo)//(dt))))
+#	index=int((b-1)+((a-1)*((xhi-xlo)//(dt))))
 #	print(a,b,index)
 #	print(c[0])
 	if (c[0]==1 or c[0]==3): grid[index,2]+=1
@@ -177,38 +149,54 @@ for i in range(0,len(grid)):
 #		fs=float(grid[i,2]/(grid[i,2]+grid[i,3]))
 #		if fs>=0.5:grid[i,4]=float(grid[i,2]/(grid[i,2]+grid[i,3]))
 #		grid[i,4]=float(math.exp(grid[i,2]/(grid[i,2]+grid[i,3])))
-		diff = float(grid[i,2] - grid[i,3])
-		if diff < 0: grid[i,4] = -1
-		elif diff == 0: grid[i,4] = 0
-		else: grid[i,4] = 1
-#		grid[i,4]=float(grid[i,2]/(grid[i,2]+grid[i,3]))
+#		diff = float(grid[i,2] - grid[i,3])
+#		if diff < 0: grid[i,4] = -1
+#		elif diff == 0: grid[i,4] = 0
+#		else: grid[i,4] = 1
+		grid[i,4]=float(grid[i,2]/(grid[i,2]+grid[i,3]))
 #		grid[i,4]=float(10**(grid[i,2]/(grid[i,2]+grid[i,3])))
 #		print(float(d[5]))
 
-"""
-matrix=np.matrix(grid)
-print(matrix)
-with open('outfile.txt','wb') as f:
-	for line in matrix:
-		np.savetxt(f, line, fmt='%.2f')
-"""
-
 l=len(xsp)
+l2=len(ysp)
+#print(l)
+
+#print(np.sum(grid,axis=0))
+#print(grid)
 #plt.hexbin(grid[:,0],grid[:,1],C=grid[:,4])
 #fig, ax = plt.hist2d(grid[:,0],grid[:,1],bins=[l,l],weights=grid[:,4])
 fig, ax = plt.subplots()
 #assign volume of the cube a colour based on ratio
-h = ax.hist2d(grid[:,0],grid[:,1],bins=l,weights=grid[:,4],vmin=-2,vmax=1,cmap='gray')
+
+#h = ax.hist2d(grid[:,0],grid[:,1],bins=[l,l2],weights=grid[:,4],vmin=-1,vmax=1) #,cmap='gray')plt.axvline(x=0.2)
+#plt.axvline(x=23.4)
+#plt.axvline(x=-3)
+#plt.axvline(x=27)
+#plt.axvline(x=10)
+#plt.axhline(y=-1.8)
+#plt.axhline(y=10)
+h = ax.hist2d(grid[:,0],grid[:,1],bins=[l-1,l2-1],weights=grid[:,4],vmin=-1,vmax=1) #,cmap='gray')
 plt.xlabel('X')
 plt.ylabel('Y')
+
+minor_ticks = xsp
+ax.set_xticks(minor_ticks, minor=True)
+ax.set_yticks(minor_ticks, minor=True)
+ax.grid(which='minor')
 cbar = plt.colorbar(h[3],ax=ax)
 cbar.set_label('Compositional variation')
-fig.suptitle('elemental composition heatmap @ Z='+str(z), fontsize=16)
+fig.suptitle('elemental composition heatmap @ Z='+str(z)+'dt='+str(dt), fontsize=16)
 #cmap=plt.cm.get_cmap('gray')
 #plt.view_colormap('viridis')
 ax.set_aspect(aspect=1)
+	#ax.set_xlim([xlo,xhi])
+	#ax.set_ylim([ylo,yhi])
 #fig1 = plt.figure(1,figsize(6.4,2))
 #plt.show()
-plt.savefig(st+'/heatmap2_'+str(z)+'.png',format='png',dpi=600)
+#string={:03d}'.format(filno)
+#string=format(sysi.argv[7],'002')
+string=filno.zfill(3)
+#print(string)
+plt.savefig(st+'/heatmap_'+string+'_'+str(dt)+'.png',format='png',dpi=600)
 
-os.remove(st+'/tmp.coords')
+#os.remove(st+'/tmp.coords')
