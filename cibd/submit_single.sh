cc36589d771d2dfe4e2e5697993888321a45ec7d
syamal.chilakalapudi <syamal.chilakalapudi@kit.edu>
2020-10-05 17:02:06 +0200

    CIBD single details updated with asphericity, energy per atom

---
+++ cibd/submit_single.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+##################################################################
+##								##
+##								##
+##Dependencies	:						##
+##Influences	:						##
+##################################################################
+## ver.	: 2019--, Syamal Praneeth Chilakalapudi, KIT, INT	##
+##Author Email    :syamalpraneeth@gmail.com			##
+##################################################################
+
+v=3
+sbatch --job-name="cibd_cuzr_"$v"nm" --output="cibd_"$v"nm_%aeV".out --export=ALL,v=$v job.arr.cibd_single
